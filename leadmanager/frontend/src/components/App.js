3837431c498a0c0fcd24b18338853476650e2716
JaGaffney <jagaffney661@msn.com>
2019-01-31 14:18:30 +1100

    website connects to django api with a react frontend, has basic CRUD functionality using redux as store manager

---
+++ leadmanager/frontend/src/components/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import ReactDom from 'react-dom'
+
+import Header from './layout/Header'
+import Dashboard from './leads/Dashboard'
+
+import { Provider } from 'react-redux'
+import store from '../store'
+
+class App extends Component {
+    render() {
+        return (
+            <Provider store={store}>
+            <>
+                <Header />
+                <div className="container">
+                    <Dashboard />
+                </div>
+
+            </>
+            </Provider>
+        )
+    }
+}
+
+ReactDom.render(<App />, document.getElementById('app'))
