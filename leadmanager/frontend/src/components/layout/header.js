85055f2135ff0cfd12a2426f4dc19396e6ea9316
JaGaffney <jagaffney661@msn.com>
2019-02-12 18:26:07 +1100

    changed title

--- leadmanager/frontend/src/components/layout/header.js
+++ leadmanager/frontend/src/components/layout/header.js
@@ -49,7 +49,7 @@ export class Header extends Component {
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                    <a className="navbar-brand" href="#">Lead Manager</a>
+                    <a className="navbar-brand" href="#">CRM</a>
                 </div>
                 {isAuthenticated ? authLinks : guestLinks}
             </div>
