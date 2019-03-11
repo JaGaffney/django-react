734912dd5ab74367be6155ebe1fb4043a659f243
JaGaffney <jagaffney661@msn.com>
2019-03-12 10:58:33 +1100

    used splitting to make the main file sizes smaller for quicker loading, still need to work out how to split per bundle so that it loads even quicker

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
+const path = require('path');
+
 module.exports = {
+    mode: 'none',
     module: {
         rules: [
             {
@@ -9,5 +12,17 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    entry: {
+        index: './leadmanager/frontend/src/index.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'leadmanager/frontend/static/frontend/')
+    },
+    optimization: { 
+        splitChunks: {
+            chunks: 'all',
+        },
+    }     
 }
